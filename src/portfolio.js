9141bc722380fafb382a155a91624f9ff3fc8881
Ashwaq Khazi <ashwaqkhazi1729@gmail.com>
2022-06-09 16:05:08 +0530

    changed github link

--- src/portfolio.js
+++ src/portfolio.js
@@ -43,7 +43,7 @@ const projects = [
     description:
       'Worked on Content-Based Recommender System. This Recommendation System will return the most similar items based on the input anime we give to it.',
     stack: ['Streamlit','scikit-learn'],
-    sourceCode: 'https://github.com/khaziashwaq/myportfoliohttps://github.com/khaziashwaq/Anime-Recommender-System',
+    sourceCode: 'https://github.com/khaziashwaq/Anime-Recommender-System',
     livePreview: 'https://khaziashwaq.github.io/myportfolio/',
   },
   
